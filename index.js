70fec3269d3dba54e403d2510c9d4f8e1851776f
Federico Brigante <github@bfred.it>
2018-01-15 18:27:10 +0700

    Make it wholly iterable

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 'use strict';
 
-module.exports = iterable => {
+module.exports = function magicIterable(iterable) {
 	return new Proxy(iterable, {
 		get(target, property) {
-			return function (...args) {
-				const ret = [];
-
-				let i = 0;
+			function * iterator() {
 				for (const item of target) {
-					i++;
+					yield item[property];
+				}
+			}
+			function * iteratorCaller(...args) {
+				for (const item of target) {
+					yield Reflect.apply(item[property], item, args);
+				}
+			}
 
-					if (typeof item[property] === 'undefined') {
-						throw new TypeError(`Item ${i} of the iterable is missing the ${property}() method`);
-					}
-
-					ret.push(Reflect.apply(item[property], item, args));
+			let i = 0;
+			let isMethod = true;
+			for (const item of target) {
+				i++;
+				if (typeof item[property] === 'undefined') {
+					throw new TypeError(`Item ${i} of the iterable is missing the ${property} property`);
 				}
 
-				return ret;
-			};
+				isMethod = isMethod && typeof item[property] === 'function';
+			}
+
+			const ret = magicIterable(isMethod ? iteratorCaller : {});
+			ret[Symbol.iterator] = iterator;
+			return ret;
 		}
 	});
 };
